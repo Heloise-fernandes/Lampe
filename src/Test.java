2a0847bb2f6e0494d5100372c2531184f1331f77
Fernandes Heloise <heloise.fernandesdealmeida@iut-valence.fr>
2014-02-03 17:35:14 +0100

    initial commit J'ai cueuilli des pommes

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test
+{
+/**Ici est la fonctions principale
+ * @param b j'aime les pattates*/
+    public static void main(String[] args)
+    {
+	if (args.length>0)
+	{
+	    System.out.println(args[0]);
+	}
+    }
+
+}
